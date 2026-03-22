5be51f1096c775df8c2cb2c475a08c2b234295d7
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-03-22 20:51:39 +0000

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)       /* Error case: negative exponent */
-        return -1;
-    if (y == 0)      /* Base case: any number to the 0 is 1 */
-        return 1;
-    return x * _pow_recursion(x, y - 1); /* Recursive step */
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
