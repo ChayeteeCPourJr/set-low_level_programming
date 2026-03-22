e2a24904b724a6a0bb3bc0125c487f08ddf02835
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-03-22 20:51:06 +0000

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,9 +8,10 @@
  */
 int factorial(int n)
 {
-    if (n < 0)        /* Error case: negative number */
-        return -1;
-    if (n == 0)       /* Base case: 0! = 1 */
-        return 1;
-    return n * factorial(n - 1); /* Recursive case */
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
 }
