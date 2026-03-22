34cbabfcd05e33cd3558d6f04e635d7cefe54431
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-03-22 20:46:40 +0000

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,12 +4,12 @@
  * _strlen_recursion - returns the length of a string
  * @s: the string to measure
  *
- * Return: the length of the string
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case: end of string */
-        return 0;
+	if (*s == '\0')
+		return (0);
 
-    return 1 + _strlen_recursion(s + 1);  /* Count current character + rest */
+	return (1 + _strlen_recursion(s + 1));
 }
