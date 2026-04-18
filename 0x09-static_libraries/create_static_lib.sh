266ad6bce5bc80df509c1f89a2675e2de604d277
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-04-18 11:26:57 +0000

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create a static library liball.a from all .c files
-
-# Compile all .c files into .o files
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-
-# Create the static library
-ar rcs liball.a *.o
