645a429d6e9077765e90a95e00391cad73e448a2
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-05-17 22:42:21 +0000

    Refactor malloc_checked for clarity and style

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *ptr;
 
+	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
 
-	return(ptr);
+	return (ptr);
 }
