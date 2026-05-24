0992c4e5dd7bdbc823f34ec0d8438bbfc62a8474
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-05-24 16:07:37 +0000

    Refactor list_len function documentation and count variable
    Updated function documentation and variable initialization.

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,14 +1,16 @@
 #include "lists.h"
 
-/*
- * list_len - Returns the number of elements in the linked list_t list
+/**
+ * list_len - Returns the number of elements in a linked list_t list
+ * @h: Pointer to the head of the list
  *
- * Return: the number of nodes in the list
+ * Return: The number of nodes in the list
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t count;
 
+	count = 0;
 	while (h != NULL)
 	{
 		count++;
