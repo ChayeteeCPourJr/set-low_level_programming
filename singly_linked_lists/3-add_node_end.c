f0d207827c767e66a0c9eff91e49a72d4a1a6426
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-05-24 16:05:05 +0000

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,37 +11,44 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    list_t *current;
+	list_t *new_node;
+	list_t *current;
+	const char *tmp;
+	unsigned int len;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+	len = 0;
+	tmp = str;
+	while (*tmp++)
+		len++;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	new_node->len = len;
+	new_node->next = NULL;
 
-    current = *head;
-    while (current->next != NULL)
-        current = current->next;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    current->next = new_node;
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
 
-    return (new_node);
+	current->next = new_node;
+
+	return (new_node);
 }
