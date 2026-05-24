20ab489cd5b6c9c1ab0c7f31c83e04d38760ca6f
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-05-24 15:58:53 +0000

    Print message before main execution

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * before_main - Prints a string before main is executed
  */
 void __attribute__((constructor)) before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
