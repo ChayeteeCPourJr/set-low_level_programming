dd660a0b761a5482f05d2b27581f42224d5b67fc
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-06-04 12:13:26 +0000

    Add files via upload

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -2,24 +2,11 @@
 #include <stdlib.h>
 
 /**
- * struct addr_node_s - node for storing visited addresses
- * @addr: address of a visited listint_t node
- * @next: points to the next node
- *
- * Description: helper struct to track visited listint_t nodes
- */
-typedef struct addr_node_s
-{
-	const listint_t *addr;
-	struct addr_node_s *next;
-} addr_node_t;
-
-/**
- * addr_seen - checks if an address has already been visited
- * @visited: head of the visited address list
+ * addr_seen - checks if address was already visited
+ * @visited: head of visited address list
  * @addr: address to look up
  *
- * Return: 1 if seen before, 0 otherwise
+ * Return: 1 if seen, 0 otherwise
  */
 static int addr_seen(addr_node_t *visited, const listint_t *addr)
 {
@@ -33,8 +20,8 @@ static int addr_seen(addr_node_t *visited, const listint_t *addr)
 }
 
 /**
- * free_addr_list - frees the entire visited address list
- * @visited: head of the visited address list
+ * free_addr_list - frees the visited address list
+ * @visited: head of visited address list
  */
 static void free_addr_list(addr_node_t *visited)
 {
@@ -55,9 +42,10 @@ static void free_addr_list(addr_node_t *visited)
 static void print_number(int n)
 {
 	unsigned int u;
-	char digits[12];
+	char buf[12];
 	int i;
 
+	i = 0;
 	if (n < 0)
 	{
 		_putchar('-');
@@ -65,22 +53,46 @@ static void print_number(int n)
 	}
 	else
 		u = (unsigned int)n;
-
-	i = 0;
 	if (u == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
 	while (u > 0)
 	{
-		digits[i++] = '0' + (u % 10);
+		buf[i++] = '0' + (u % 10);
 		u /= 10;
 	}
-
 	while (i > 0)
-		_putchar(digits[--i]);
+		_putchar(buf[--i]);
+}
+
+/**
+ * add_to_visited - adds a node address to visited list
+ * @visited: pointer to head of visited list
+ * @tail: pointer to tail of visited list
+ * @addr: address to record
+ */
+static void add_to_visited(addr_node_t **visited,
+		addr_node_t **tail, const listint_t *addr)
+{
+	addr_node_t *node;
+
+	node = malloc(sizeof(addr_node_t));
+	if (node == NULL)
+		exit(98);
+	node->addr = addr;
+	node->next = NULL;
+	if (*visited == NULL)
+	{
+		*visited = node;
+		*tail = node;
+	}
+	else
+	{
+		(*tail)->next = node;
+		*tail = node;
+	}
 }
 
 /**
@@ -94,48 +106,28 @@ size_t print_listint_safe(const listint_t *head)
 	size_t count;
 	addr_node_t *visited;
 	addr_node_t *tail;
-	addr_node_t *new_node;
 
 	count = 0;
 	visited = NULL;
 	tail = NULL;
-
 	while (head != NULL)
 	{
 		if (addr_seen(visited, head))
 		{
+			_putchar('-');
+			_putchar('>');
+			_putchar(' ');
+			print_number(head->n);
+			_putchar('\n');
 			free_addr_list(visited);
-			exit(98);
+			return (count);
 		}
-
-		new_node = malloc(sizeof(addr_node_t));
-		if (new_node == NULL)
-			exit(98);
-
-		new_node->addr = head;
-		new_node->next = NULL;
-
-		if (visited == NULL)
-		{
-			visited = new_node;
-			tail = new_node;
-		}
-		else
-		{
-			tail->next = new_node;
-			tail = new_node;
-		}
-
-		_putchar('[');
+		add_to_visited(&visited, &tail, head);
 		print_number(head->n);
-		_putchar(']');
 		_putchar('\n');
-
 		count++;
 		head = head->next;
 	}
-
 	free_addr_list(visited);
-
 	return (count);
 }
