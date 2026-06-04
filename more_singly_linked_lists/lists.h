980841fb81987b39946ec2486beded3851070355
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-06-04 12:03:33 +0000

    Define addr_node_s for visited address tracking
    Added addr_node_s structure for tracking visited nodes.

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -24,6 +24,40 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+#endif /* LISTS_H */
+
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct addr_node_s - node for storing visited addresses
+ * @addr: address of a visited listint_t node
+ * @next: points to the next addr_node_t
+ *
+ * Description: helper struct to track visited nodes
+ */
+typedef struct addr_node_s
+{
+	const listint_t *addr;
+	struct addr_node_s *next;
+} addr_node_t;
+
 int        _putchar(char c);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
