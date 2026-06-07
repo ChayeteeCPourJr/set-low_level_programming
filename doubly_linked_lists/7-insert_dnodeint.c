a605f207c0ae4739b783c0ea0296159742d91b09
Chea C Pour Jr. <chayeteecpourjr@gmail.com>
2026-06-07 23:23:33 +0000

    Fix index variable type and improve comments

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to the pointer of the head of the list
- * @idx: index where the new node should be inserted, starting from 0
+ * @idx: index where the new node should be inserted, starting at 0
  * @n: integer to store in the new node
  *
  * Return: address of the new node, or NULL if it failed
@@ -12,34 +12,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *tmp;
-	unsigned i;
+	unsigned int i;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	tmp = *h;
-	for (i = 0; tmp != NULL; ++i)
+	for (i = 0; tmp != NULL; i++)
 	{
-		if (i == idx -1)
+		if (i == idx - 1)
 		{
 			if (tmp->next == NULL)
-				return (add_dnodeint(h, n));
-
+				return (add_dnodeint_end(h, n));
 			new = malloc(sizeof(dlistint_t));
 			if (new == NULL)
 				return (NULL);
-
 			new->n = n;
 			new->next = tmp->next;
 			new->prev = tmp;
 			tmp->next->prev = new;
 			tmp->next = new;
-
 			return (new);
 		}
-
 		tmp = tmp->next;
 	}
-
 	return (NULL);
 }
